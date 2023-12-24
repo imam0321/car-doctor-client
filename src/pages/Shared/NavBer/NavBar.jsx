9141c25abaf34ee0c5bbe1c200f167f678da92b7
imam0321 <imam.hossain0321@gmail.com>
2023-12-24 20:07:59 +0600

    added login and signUp components and authprovider component

--- src/pages/Shared/NavBer/NavBar.jsx
+++ src/pages/Shared/NavBer/NavBar.jsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.svg";
+import { useContext } from "react";
+import { AuthContext } from "../../../Providers/AuthProvider";
 
 const NavBar = () => {
+  const {user, logOut} = useContext(AuthContext);
+  const handleLogOut = () =>{
+    logOut()
+    .then(() => {})
+    .catch(error => console.log(error))
+  }
   const navItems = (
     <>
       <li>
@@ -11,11 +19,12 @@ const NavBar = () => {
         <Link to='/about'>About</Link>
       </li>
       <li>
-        <Link to='/login'>Login</Link>
-      </li>
-      <li>
         <Link to='/blogs'>Blogs</Link>
       </li>
+      {
+        user ? <li><Link onClick={handleLogOut}>LogOut</Link></li> : 
+        <li><Link to='/login'>Login</Link></li>
+      }
     </>
   );
   return (
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,20 +1,22 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import login from '../../assets/images/login/login.svg'
-import { useContext } from 'react';
-import { AuthContext } from '../../Providers/AuthProvider';
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
 
-const Login = () => {
-  const { signIn } = useContext(AuthContext);
-  const navigate = useNavigate()
-  const handleLogin = (event) => {
+const SignUp = () => {
+  const { createUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSignUp = (event) => {
     event.preventDefault();
     const form = event.target;
+    const name = form.name.value;
     const email = form.email.value;
     const password = form.password.value;
-    const user = {email, password};
+    const user = {name, email, password};
     console.log(user);
 
-    signIn(email, password)
+    createUser(email, password)
     .then(result => {
       const user = result.user;
       navigate('/');
@@ -24,22 +26,35 @@ const Login = () => {
       console.log(error);
     })
   }
+
   return (
-    <div className="hero min-h-screen bg-base-200 -mt-12">
+    <div className="hero min-h-screen bg-base-200 ">
       <div className="hero-content flex-col lg:flex-row">
         <div className="w-1/2 mr-12">
           <img src={login} alt="" />
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleLogin} className="card-body">
-          <h1 className="text-5xl text-center font-bold">Login now!</h1>
+          <form onSubmit={handleSignUp} className="card-body">
+            <h1 className="text-5xl text-center font-bold">Sign Up now!</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                className="input input-bordered"
+                required
+              />
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
-                name='email'
+                name="email"
                 placeholder="email"
                 className="input input-bordered"
                 required
@@ -47,37 +62,37 @@ const Login = () => {
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text">Password</span>
+                <span className="label-text">Confirm Password</span>
               </label>
               <input
                 type="password"
-                name='password'
-                placeholder="password"
+                name="password"
+                placeholder="Confirm password"
                 className="input input-bordered"
                 required
               />
-              <label className="label">
+              {/* <label className="label">
                 <a href="#" className="label-text-alt link link-hover">
                   Forgot password?
                 </a>
-              </label>
+              </label> */}
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button className="btn btn-primary">Sign Up</button>
             </div>
             <label className="label">
-              <p>New to Car Doctor?{" "}
-                <Link to='/sign-up' className="label-text-alt link link-hover">
-                  Sign Up
+              <p>
+                Already have an account?{" "}
+                <Link to="/login" className="label-text-alt link link-hover">
+                  Login
                 </Link>
               </p>
             </label>
           </form>
-          
         </div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default SignUp;
